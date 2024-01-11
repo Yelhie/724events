bc9cdf32995bf32b0cdefb0224d14bac5916b490
Yelhie <maelle.ew.m@gmail.com>
2024-01-11 11:16:04 +0100

    Correction warning console au niveau du Slider
    - Modification du nom des keys afin d'éviter d'avoir des Key identique
    - Modification de key={event.title}.  Ajout d'une div pour directement liée la Key au contenu de chaque élément du slider.
    - Ajout de la prop readOnly au bouton radio afin de le passer en lecture seule le checked

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -16,19 +16,18 @@ const Slider = () => {
       5000
     );
   };
+
   useEffect(() => {
     nextCard();
   });
-  return (
+  return (    
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
+         <div key={event.title} >
+         <div className={`SlideCard SlideCard--${
+                 index === idx ? "display" : "hide"
+             }`}
+         >
             <img src={event.cover} alt="forum" />
             <div className="SlideCard__descriptionContainer">
               <div className="SlideCard__description">
@@ -37,20 +36,21 @@ const Slider = () => {
                 <div>{getMonth(new Date(event.date))}</div>
               </div>
             </div>
-          </div>
-          <div className="SlideCard__paginationContainer">
+         </div>
+         <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
+              {byDateDesc.map((radioDot, radioIdx) => (
                 <input
-                  key={`${event.id}`}
+                  key={`${radioDot.title}`}
                   type="radio"
                   name="radio-button"
                   checked={index === radioIdx}
+                  readOnly
                 />
               ))}
             </div>
-          </div>
-        </>
+         </div>
+        </div>
       ))}
     </div>
   );
